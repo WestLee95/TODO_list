909f75191e16d18479fdd54bba272bdde6cd3317
Eugene <eugenewestley95@gmail.com>
2025-06-06 15:12:49 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Todo List</h1>
+      <h1>What's On Your Schedule?</h1>
       <TodoForm addTodo={addTodo} />
       <TodoList todos={todos} removeTodo={removeTodo} />
     </div>
